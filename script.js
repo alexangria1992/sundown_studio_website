9a1cea33b5d167dc875b8d9e2bb58c6de6eab5fc
Alexandre Angrignon <alex040892@gmail.com>
2024-08-01 12:34:39 +1200

    javascript image popup

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector('#main'),
   smooth: true,
 });
+
+// let a = document.querySelector('#elem1');
+// var image = a.getAttribute('data-image');
+// console.log(image);
+
+var elemC = document.querySelector('#elem-container');
+var fixed = document.querySelector('#fixed-image');
+var elem1 = document.querySelector('#elem1');
+elemC.addEventListener('mouseenter', () => {
+  // console.log('mouse over');
+  fixed.style.display = 'block';
+});
+
+elemC.addEventListener('mouseleave', () => {
+  // console.log('mouse over');
+  fixed.style.display = 'none';
+});
+
+var elems = document.querySelectorAll('.elem');
+elems.forEach((e) => {
+  // console.log(e);
+  e.addEventListener('mouseenter', () => {
+    var image = e.getAttribute('data-image');
+    fixed.style.backgroundImage = `url(${image})`;
+  });
+});
